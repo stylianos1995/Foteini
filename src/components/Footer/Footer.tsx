d11eb5941369bbcf8c9c028c4be2febc6c0cced0
Stelios Kalaitzis <stylianos.kalaitzis95@gmail.com>
2025-11-09 11:30:40 +0100

    ix lint warnings before deploy

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Footer.css";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import { Link } from "react-router-dom";
 import { useLanguage } from "../../contexts/LanguageContext";
